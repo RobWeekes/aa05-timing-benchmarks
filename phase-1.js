76db230b7249ec12d1d5fbdc14a82adf2fa0d6c2
Robert Weekes <weekes.robert@gmail.com>
2024-06-18 23:01:22 -0700

    passing 11/11, phase 1

--- phase-1.js
+++ phase-1.js
@@ -1,16 +1,36 @@
 // Adds up positive integers from 1-n
 function addNums(n) {
-  // Fill this in
-
+    let sum = 0;
+    for(let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
 }
 
 
 // Adds up values of addNums(1) through addNums(n)
 function addManyNums(n) {
-  // Fill this in
-
+    let sum = 0;
+    for(let i = 1; i <= n; i++) {
+        sum += addNums(i);
+    }
+    return sum;
 }
 
 
+// local testing
+
+console.log(addNums(5)); // 15
+console.log(addNums(10)); // 55
+
+console.log(addManyNums(3)); // 10
+console.log(addNums(10000)); // 50005000
+
+const startTime = Date.now();
+addNums(10000000);
+const endTime = Date.now();
+
+const runtime = endTime - startTime;
+console.log(`Runtime: ${runtime}`)
 
 module.exports = [addNums, addManyNums];
